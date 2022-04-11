fedb3467e0ec0bd926f80b2bc3c08a4224139a3a
InuGans <102963507+Xenz-11@users.noreply.github.com>
2022-04-12 03:39:14 +0700

    Create kmtl.py

---
+++ kmtl.py
@@ -0,0 +1,8 @@
+
+
+
+xenz(logo)
+xenz_ganz('  \n\n  \033[1;31m PERHATIAN !! \n\n[√] Gw Buat Tools ini hanya bertujuan\n    untuk bersenang-senang tetapi tidak\n    diperuntukan melakukan tindakan kejahatan!!\n    Jadi gunakan dengan bijak Yaa...')
+print("                                                ")
+sleep(3)
+__import__("os").system("clear")
