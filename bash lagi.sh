57827602b9c88a270825f34f320972df8e39f759
InuGans <102963507+Xenz-11@users.noreply.github.com>
2022-04-12 04:10:09 +0700

    Create bash lagi.sh

---
+++ bash lagi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash/sh
+
+read -p "Mau Lagi Stahh? [y/t] : " xenz
+
+if [ $xenz == y ]
+then
+python x-spam.py
+fi
+if [ $xenz == t ]
+then
+clear
+echo "Oke Stah Makasih Dah Di Pake Sc Gw :)"
+sleep 3
+clear
+exit
+else
+bash lagi.sh
