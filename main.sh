c76f57d3692c5d6eb728167a5b30e51d5be7cce7
InuGans <102963507+Xenz-11@users.noreply.github.com>
2022-04-12 13:41:26 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,32 @@
 #!/bin/bash/sh
 git pull
 clear
+
+
+read -p "Sudah Install Bahan Belum Stah... [s/b]" xenz
+if [ $xenz == s ]
+then
+echo "Oke Stah..."
+sleep 2
+clear
 python2 code.py
 python kmtl.py
 python mwmwk.py
 python x-spam.py
 bash lagi.sh
+fi
+if [ $xenz == b ]
+then
+clear
+pkg install python
+pkg install python2
+pip install requests
+clear
+python2 code.py
+python kmtl.py
+python mwmwk.py
+python x-spam.py
+bash lagi.sh
+else
+echo "Pilihan Tidak Tersedia"
+fi
